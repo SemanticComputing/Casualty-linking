13cb63297cb96f455dca87ffbfb0b5142f8ff4bb
Mikko Koho <mikko.koho@aalto.fi>
2016-10-21 11:15:18 +0300

    Add missing steps to conversion shell script

--- full_process.sh
+++ full_process.sh
@@ -13,3 +13,11 @@ python sotasampo_helpers/arpa.py units candidates data/new/surma.ttl data/new/su
 python sotasampo_helpers/arpa.py units join data/new/surma_unit_candidates.ttl data/new/unit_candidates_combined.ttl http://candidates/unit http://localhost:3030/warsa/sparql  --prop http://candidates/unit -n -w 3 -r 3
 python sotasampo_helpers/arpa.py units disambiguate SPARQL/arpa_menehtyneet_units.sparql data/surma_ranks.ttl data/new/unit_full_combined.ttl data/new/surma_units.ttl  http://ldf.fi/schema/narc-menehtyneet1939-45/osasto http://localhost:3030/warsa/sparql  --prop http://candidates/unit -n -w 3 -r 3
 
+cd data/new
+
+# cp person_linked.ttl person_linked_manual_fixes.ttl  # for manual adjusting if needed
+# cat surma.ttl person_linked_manual_fixes.ttl surma_units.ttl > surma_with_links_2.ttl
+
+cat surma.ttl person_linked.ttl surma_units.ttl > surma_with_links.ttl
+
+python tasks.py documents_links data/new/surma_with_links.ttl data/new/surma_final.ttl --endpoint http://localhost:3030/warsa/sparql
