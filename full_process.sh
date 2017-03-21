474d917184cc6bc16bf0e74e5d467f5ba4c1293d
Erkki Heino <erkki.heino@aalto.fi>
2017-03-21 15:40:02 +0200

    Link units using existing person links

--- full_process.sh
+++ full_process.sh
@@ -9,15 +9,16 @@ python sotasampo_helpers/arpa.py persons join data/new/surma_person_candidates.t
 cat surma.ttl person_candidates_combined.ttl > person_full_combined.ttl
 python sotasampo_helpers/arpa.py persons disambiguate SPARQL/arpa_menehtyneet_persons.sparql data/surma_ranks.ttl data/new/person_full_combined.ttl data/new/person_linked.ttl  http://www.cidoc-crm.org/cidoc-crm/P70_documents http://localhost:3030/warsa/sparql  --prop http://candidates/person -n -w 3 -r 3
 
-python sotasampo_helpers/arpa.py units candidates data/new/surma.ttl data/new/surma_unit_candidates.ttl http://candidates/unit http://demo.seco.tkk.fi/arpa/menehtyneet_units  --prop http://ldf.fi/schema/narc-menehtyneet1939-45/joukko_osasto -n -w 3 -r 3
-python sotasampo_helpers/arpa.py units join data/new/surma_unit_candidates.ttl data/new/unit_candidates_combined.ttl http://candidates/unit http://localhost:3030/warsa/sparql  --prop http://candidates/unit -n -w 3 -r 3
-python sotasampo_helpers/arpa.py units disambiguate SPARQL/arpa_menehtyneet_units.sparql data/surma_ranks.ttl data/new/unit_full_combined.ttl data/new/surma_units.ttl  http://ldf.fi/schema/narc-menehtyneet1939-45/osasto http://localhost:3030/warsa/sparql  --prop http://candidates/unit -n -w 3 -r 3
-
 cd data/new
 
 # cp person_linked.ttl person_linked_manual_fixes.ttl  # for manual adjusting if needed
 # cat surma.ttl person_linked_manual_fixes.ttl surma_units.ttl > surma_with_links_2.ttl
 
-cat surma.ttl person_linked.ttl surma_units.ttl > surma_with_links.ttl
+cat surma.ttl person_linked.ttl > surma_with_links.ttl
 
-python tasks.py documents_links data/new/surma_with_links.ttl data/new/surma_final.ttl --endpoint http://localhost:3030/warsa/sparql
+python tasks.py documents_links surma_with_links.ttl surma_wo_units.ttl --endpoint http://localhost:3030/warsa/sparql
+
+echo 'query=' | cat - unit_construct.sparql | sed 's/&/%26/g' | curl -d @- http://localhost:3030/warsa/sparql > surma_units.ttl
+
+cat surma_wo_units.ttl surma_units.ttl > surma_final.ttl
+
