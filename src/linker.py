fd8cb19a4941132014dc1a778d01217657523213
Mikko Koho <mikko.koho@aalto.fi>
2018-07-03 16:40:33 +0300

    Harmonize common occupation spellings

--- src/linker.py
+++ src/linker.py
@@ -286,7 +286,7 @@ def main():
     elif args.task == 'occupations':
         log.info('Linking occupations')
         bind_namespaces(link_occupations(input_graph, args.endpoint, CASUALTY_MAPPING['AMMATTI']['uri'],
-                                         BIOC.has_occupation, SCHEMA_WARSA.DeathRecord)) \
+                                         BIOC.has_occupation, SCHEMA_WARSA.DeathRecord, sep=',')) \
             .serialize(args.output, format=guess_format(args.output))
 
 
